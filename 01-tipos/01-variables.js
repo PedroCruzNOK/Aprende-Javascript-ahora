1333ce9332617e22881bea8b63dbe502676430f6
“Pedro-Cruz” <cruzrodriguezpedroj@gmail.com>
2024-07-16 12:33:36 -0600

    ✨ Se crean archivos  de documentacion basica

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,10 @@
+let nombre = 'Hola mundo';
+//convenciones para definir variables UpperCamelCase, camelCase, snake_case
+let NombreCompleto; //la canvencion dicta que se ocupe UpperCamelCase para constantes.
+let nombreCompleto; // camelCase es la convencion mas usada
+let nombre_completo;
+console.log(nombre);
+
+let apellido; //inicializacion de la variable vacia
+apellido = 'cruz'; //posteriormente se le asignaun valor asignacion
+
---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,16 @@
+//comentario
+
+/*
+todo esto 
+se ignora
+*/
+
+/**
+ * Este es un parrafo de comentarios multilinea
+ * muchas
+ * lineas
+ */
+
+let numero = 42; // este es un comentario
+
+console.log(numero);
